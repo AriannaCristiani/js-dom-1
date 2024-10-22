5ba85d17630591c9eb13d7b914126dea1145f8bc
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-22 14:56:24 +0200

    creazione file con aggiunta immagine e bottone

---
+++ app.js
@@ -0,0 +1,5 @@
+//Inserisco in HTML l’immagine della lampadina spenta e accanto 
+//aggiungo un bottone con la scritta “Accendi”.
+
+//Al click del bottone, la lampadina dovrà accendersi,
+// inserirò dunque la seconda immagine con la lampadina accesa
