d9eb480c703a7d3f407688abc4a90c48b84c02e1
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-22 16:43:03 +0200

    aggiunta funzione sul bottone per cambiarne la dicitura quando la lampadina è accesa

--- app.js
+++ app.js
@@ -4,6 +4,11 @@
 //Al click del bottone, la lampadina dovrà accendersi,
 // inserirò dunque la seconda immagine con la lampadina accesa
 
+//bonus
+//quando la lampadina è spenta, il bottone avrà su scritto 'accendi'
+//quando la lampadina è accesa, il bottone avrà su scritto 'spegni'
+//drovrò dunque ripetere la procedura al contrario e cambiare la dicitura sul bottone
+
 
 const witheLamp = document.getElementById('white-lamp');
 // console.log(witheLamp)
@@ -18,5 +23,6 @@ button.addEventListener('click',  function() {
    console.log("l'utente ha cliccato sul bottone")
    witheLamp.classList.add ('hidden')
    yellowLamp.classList.remove ('hidden')
-
+   button.innerHTML = 'Spegni la lampadina'
 })
+
