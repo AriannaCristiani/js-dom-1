35624cc890bc4a905d5b8a029f5602d5fa66c740
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-22 15:55:30 +0200

    aggiunta funzione sul bottone

--- app.js
+++ app.js
@@ -3,3 +3,16 @@
 
 //Al click del bottone, la lampadina dovrà accendersi,
 // inserirò dunque la seconda immagine con la lampadina accesa
+
+
+const witheLamp = document.getElementById('white-lamp');
+// console.log(witheLamp)
+
+const button = document.getElementById('btn');
+// console.log(button)
+
+button.addEventListener('click',  function() {
+
+
+   console.log("l'utente ha cliccato sul bottone")
+})
