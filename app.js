f810b5321848db5e0f5ffe20b55d70c689c89b8f
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-23 18:28:00 +0200

    correzione esercizio con aggiunta del bonus

--- app.js
+++ app.js
@@ -13,16 +13,22 @@
 const witheLamp = document.getElementById('white-lamp');
 // console.log(witheLamp)
 
-const yellowLamp = document.getElementById('yellow-lamp');
-// console.log(yellowLamp)
-
 const button = document.getElementById('btn');
 // console.log(button)
 
-button.addEventListener('click',  function() {
-   console.log("l'utente ha cliccato sul bottone")
-   witheLamp.classList.add ('hidden')
-   yellowLamp.classList.remove ('hidden')
-   button.innerHTML = 'Spegni la lampadina'
-})
 
+button.addEventListener('click',  function() {
+    console.log("l'utente ha cliccato sul bottone")
+    witheLamp.src = 'img/yellow_lamp.png'
+    witheLamp.alt = 'lampadina accesa'
+    
+
+    if (witheLamp.classList.contains('off')){
+        witheLamp.classList.remove('off')
+        button.innerText = 'Spegni la lampadina'
+    } else {
+        witheLamp.classList.add('off')
+        button.innerText = 'Accendi la lampadina'
+    }
+    
+ })
